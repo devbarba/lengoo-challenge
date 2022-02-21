d6863ab3ab59cd5d37f7994e284c631f3ee4da7c
Joao Harbs <harbspj@gmail.com>
2022-02-21 21:59:01 +0100

    feat: calling job to upload file

--- src/controllers/subtitles.controller.ts
+++ src/controllers/subtitles.controller.ts
@@ -1,4 +1,6 @@
 import { IRoute, IResponse } from '@interfaces/route';
+import { ISubtitle } from '@interfaces/subtitle';
+import { Queue } from '@queue';
 import { OK } from 'http-status';
 
 interface ISubtitleController {
@@ -8,6 +10,22 @@ interface ISubtitleController {
 class SubtitleController implements ISubtitleController {
     public async create({ req, res, next }: IRoute): Promise<IResponse<void>> {
         try {
+            const languages: ISubtitle = req.body;
+
+            if (req.files) {
+                // @ts-ignore
+                req.files.forEach((file) => {
+                    Queue.create('subtitles_upload', {
+                        sourceLanguage: languages.sourceLanguage,
+                        targetLanguage: languages.targetLanguage,
+                        file,
+                        _user: req.user.id,
+                    })
+                        .removeOnComplete(true)
+                        .save();
+                });
+            }
+
             return res.status(OK).json({
                 msg: 'subtitles sent to translate, you will receive in your registered email when we finish the job',
             });
