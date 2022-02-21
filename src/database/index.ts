15c062f9c2519fbd7f9d2eac9367276016444e4a
Joao Harbs <harbspj@gmail.com>
2022-02-21 10:08:12 +0100

    feat: changing mongo connection to mongoose and envs

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,23 +1,13 @@
-import { createConnection, Connection, ConnectionOptions } from 'typeorm';
+import mongoose from 'mongoose';
 
 import IConfig from '../interfaces/configs';
-import User from '../models/User';
 
-export default async (
-    database: IConfig['app']['database']
-): Promise<Connection> => {
-    const options: ConnectionOptions = {
-        name: 'default',
-        logging: true,
-        synchronize: true,
-        type: database.type,
-        host: database.host,
-        port: database.port,
-        username: database.user,
-        password: database.pass,
-        database: database.name,
-        entities: [User],
-    };
-
-    return createConnection(options);
+export default async (database: IConfig['app']['database']): Promise<void> => {
+    mongoose
+        .connect(`mongodb+srv://${database.host}`, {
+            dbName: database.name,
+            user: database.user,
+            pass: database.pass,
+        })
+        .catch(() => process.exit(1));
 };
