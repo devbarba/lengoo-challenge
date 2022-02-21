1e4742a2251ae7ef450826608e590693f790d406
Joao Harbs <harbspj@gmail.com>
2022-02-21 14:37:33 +0100

    chore: updating models timestamp way

--- src/models/Translation.ts
+++ src/models/Translation.ts
@@ -5,33 +5,36 @@ import { ITranslation } from '../interfaces/translation';
 
 interface ITranslationModel extends ITranslation, Document {}
 
-const Translation = new Schema({
-    _user: {
-        type: ObjectId,
-        ref: 'User',
-        required: true,
+const Translation = new Schema(
+    {
+        _user: {
+            type: ObjectId,
+            ref: 'User',
+            required: true,
+        },
+        source: {
+            type: String,
+            required: true,
+        },
+        target: {
+            type: String,
+            required: true,
+        },
+        sourceLanguage: {
+            type: String,
+            min: 2,
+            max: 2,
+            required: true,
+        },
+        targetLanguage: {
+            type: String,
+            min: 2,
+            max: 2,
+            required: true,
+        },
     },
-    source: {
-        type: String,
-        required: true,
-    },
-    target: {
-        type: String,
-        required: true,
-    },
-    sourceLanguage: {
-        type: String,
-        min: 2,
-        max: 2,
-        required: true,
-    },
-    targetLanguage: {
-        type: String,
-        min: 2,
-        max: 2,
-        required: true,
-    },
-});
+    { timestamps: true }
+);
 
 const TranslationModel: Model<ITranslationModel> = model<
     ITranslationModel,
