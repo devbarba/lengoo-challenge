5309effb713f3d57e4d32f738b76694fe245dfd9
Joao Harbs <harbspj@gmail.com>
2022-02-21 14:38:09 +0100

    feat: creating service to insert translations

---
+++ src/services/translation.service.ts
@@ -0,0 +1,21 @@
+import { ITranslation } from '../interfaces/translation';
+import Translation from '../models/Translation';
+
+interface ITranslationService {
+    insert(translations: ITranslation[]): Promise<ITranslation[]>;
+}
+
+class TranslationService implements ITranslationService {
+    public async insert(translations: ITranslation[]): Promise<ITranslation[]> {
+        return Promise.all(
+            translations.map((translation) =>
+                Translation.findOneAndUpdate(translation, translation, {
+                    upsert: true,
+                    new: true,
+                })
+            )
+        );
+    }
+}
+
+export default TranslationService;
