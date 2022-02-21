f13a79566d46c518236887c3762a2c132b9343c2
Joao Harbs <harbspj@gmail.com>
2022-02-21 18:52:26 +0100

    feat: creating subtitles queue structure and refactoring app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,31 @@
 import createConnection from '@database';
 import Handler from '@errors/handler.error';
 import IConfig from '@interfaces/configs';
+import { loadQueueUi, loadCron } from '@queue';
 import routes from '@routes/index';
 import { autoloadConfig, getBaseDir } from '@utils/helper';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import express, { Application } from 'express';
 import fileUpload from 'express-fileupload';
-import kue from 'kue';
-import kueUi from 'kue-ui';
-import cronJob from 'node-cron';
 
 const Minio = require('minio');
 
 class App {
     public server: Application;
     public configObject: IConfig;
-    public minio;
+    public minio: unknown;
 
     constructor() {
         this.loadConfigurations();
         this.server = express();
-        this.queues();
         this.middlewares();
         this.routes();
-        this.crons();
         this.mongoDatabase();
         this.minioBucket();
         this.errorHandling();
+        loadCron();
+        loadQueueUi(this.server);
     }
 
     private loadConfigurations() {
@@ -38,8 +36,6 @@ class App {
         this.server.use(cors());
         this.server.use(fileUpload());
         this.server.use(bodyParser.json());
-        this.server.use('/queues', kueUi.app);
-        this.server.use('/queues/api', kue.app);
         this.server.use(
             bodyParser.urlencoded({
                 extended: true,
@@ -49,24 +45,6 @@ class App {
 
     private routes() {
         this.server.use('/api', routes);
-    }
-
-    public queues() {
-        kue.createQueue({
-            redis: {
-                host: this.configObject.app.redis.host,
-                port: this.configObject.app.redis.port,
-                auth: this.configObject.app.redis.pass,
-            },
-        });
-        kueUi.setup({
-            apiURL: '/queues/api',
-            baseURL: '/queues',
-        });
-    }
-
-    private crons() {
-        // cronJob.schedule('* * * * *', () => {});
     }
 
     private mongoDatabase() {
---
+++ src/queue/index.ts
@@ -0,0 +1,38 @@
+import { getEnv } from '@utils/helper';
+import dotenv from 'dotenv-safe';
+import { Application } from 'express';
+import kue, { Job, DoneCallback } from 'kue';
+import kueUi from 'kue-ui';
+import cronJob from 'node-cron';
+
+dotenv.config();
+
+const Queue = kue.createQueue({
+    redis: {
+        host: getEnv('REDIS_HOST', '', true),
+        port: getEnv('REDIS_PORT', '', true),
+        auth: getEnv('REDIS_PASS', '', true),
+    },
+});
+
+Queue.process('subtitles_upload', 1, (job: Job, done: DoneCallback) => {
+    console.log('running');
+    done();
+});
+
+const loadQueueUi = (server: Application) => {
+    server.use('/queues/', kueUi.app);
+    server.use('/queues/api', kue.app);
+    kueUi.setup({
+        apiURL: '/queues/api',
+        baseURL: '/queues',
+    });
+};
+
+const loadCron = () => {
+    cronJob.schedule('* * * * *', () => {
+        Queue.create('subtitles_upload', 1).save();
+    });
+};
+
+export { Queue, loadQueueUi, loadCron };
