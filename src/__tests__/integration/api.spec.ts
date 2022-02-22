b5f06802badcfafe81b47e35914eece7d2d4f388
Joao Harbs <harbspj@gmail.com>
2022-02-22 18:17:18 +0100

    test: first testes from [GET] - /api healthcheck

---
+++ src/__tests__/integration/api.spec.ts
@@ -0,0 +1,33 @@
+import app from '@app';
+import * as dotenv from 'dotenv-safe';
+import { OK } from 'http-status';
+import mongoose from 'mongoose';
+import request from 'supertest';
+
+describe('[GET] - /api', () => {
+    const endpointToCall = '/api';
+
+    beforeAll(() => {
+        process.env.NODE_ENV = 'test';
+        dotenv.config();
+    });
+
+    beforeAll(async () => {
+        await mongoose.disconnect();
+    });
+
+    test('This should load with no errors', async () => {
+        expect.assertions(2);
+        const response = await request(app.server).get(endpointToCall);
+        expect(response.status).toBe(OK);
+        expect(response.body).toMatchObject({
+            timestamp: expect.any(String),
+        });
+    });
+
+    test('This should allow cors', async () => {
+        expect.assertions(1);
+        const response = await request(app.server).options(endpointToCall);
+        expect(response.header['access-control-allow-origin']).toBe('*');
+    });
+});
